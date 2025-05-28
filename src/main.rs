ec4979f43a9eeefd3fa7a92ac3e5912163cafa28
Matthew Yang <matthew.yang204@gmail.com>
2025-05-28 10:34:09 +0800

    Ad usage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process::exit;
 
 fn getArgs() {
 	let args: Vec<String> = env::args().collect();
@@ -6,9 +7,17 @@ fn getArgs() {
 }
 
 fn help() {
-	
+	println!("Usage: gpgde OPTIONS filename");
+	println!("-d, --decrypt     Decrypt file specified");
+	println!("-e, --encrypt     Encrypt file specified");
+	println!("filename          Name of file");
 }
 
 fn main() {
 	let args = getAgs();
+	
+	if args.len() != 2 {
+		help();
+		exit(1);
+	}
 }
