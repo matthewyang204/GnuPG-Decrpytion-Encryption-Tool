5f7eb771e00b11ec260205221d2b77e3b77074f7
Matthew Yang <matthew.yang204@gmail.com>
2025-05-28 15:56:20 +0800

    Add a gpg file extension stripping function to decrypter

--- src/decrypter.rs
+++ src/decrypter.rs
@@ -1,4 +1,16 @@
+use std::path::{Path, PathBuf};
+
+fn strip_gpg_extension(filename: &str) -> String {
+    let path = Path::new(filename);
+    if let Some(stem) = path.file_stem() {
+        let mut new_path = PathBuf::from(path);
+        new_path.set_extension("");
+        return new_path.to_string_lossy().to_string();
+    }
+    filename.to_string()
+}
+
 pub fn decrypt(file: &str) {
-	println!("Not implemented yet, but got file {}", file);
-	exit(1);
+    let stripped_file = strip_gpg_extension(file);
+	println!("Not implemented yet, but got file {}, and the stripped filename is {}", file, stripped_file);
 }
--- src/encrypter.rs
+++ src/encrypter.rs
@@ -1,4 +1,3 @@
 pub fn encrypt(file: &str) {
 	println!("Not implemented yet, but got file {}", file);
-	exit(1);
 }
